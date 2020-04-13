8114b133b42127b3d9f2ea11aaba487e6e829557
LEE KANG HO <ho7719@gmail.com>
2020-04-13 08:45:19 +0000

    update codes for v2qq test

--- v2qq_test.sh
+++ v2qq_test.sh
@@ -11,17 +11,17 @@ do
                 for layer in 20 32 44
                 do
                     python3 find_similar_kernel.py $data -a $model --layers $layer --ckpt ckpt_best.pth -v v2qq --qb $qb
-                    python3 main.py $data -a $model --layers $layer -j 4 -C -g 0 -E -N --ckpt "ckpt_best_v2qq_q"$qb".pth" --qb $qb
-                    python3 main.py $data -a $model --layers $layer -j 4 -C -g 0 1 -b 256 -T --ckpt "ckpt_best_v2qq_q"$qb".pth" -N -v v2qq --qb $qb --lr 0.01
+                    python3 main.py $data -a $model --layers $layer -j 4 -C -g 0 -E -N --ckpt "ckpt_best_v2qq_q"$qb"88.pth" --qb $qb
+                    python3 main.py $data -a $model --layers $layer -j 4 -C -g 0 1 -b 256 -T --ckpt "ckpt_best_v2qq_q"$qb"88.pth" -N -v v2qq --qb $qb --lr 0.01
                     rm -f checkpoint/*/*/ckpt_rt*_v*_q*_epoch_*.pth
-                    python3 main.py $data -a $model --layers $layer -j 4 -C -g 0 -E -N --ckpt "ckpt_rt1_v2qq_q"$qb"_best.pth" --qb $qb
+                    python3 main.py $data -a $model --layers $layer -j 4 -C -g 0 -E -N --ckpt "ckpt_rt1_v2qq_q"$qb"88_best.pth" --qb $qb
                 done
             else
                 python3 find_similar_kernel.py $data -a $model --ckpt ckpt_best.pth -v v2qq --qb $qb
-                python3 main.py $data -a $model -j 4 -C -g 0 -E -N --ckpt "ckpt_best_v2qq_q"$qb".pth" --qb $qb
-                python3 main.py $data -a $model -j 4 -C -g 0 1 -b 256 -T --ckpt "ckpt_best_v2qq_q"$qb".pth" -N -v v2qq --qb $qb --lr 0.01
+                python3 main.py $data -a $model -j 4 -C -g 0 -E -N --ckpt "ckpt_best_v2qq_q"$qb"88.pth" --qb $qb
+                python3 main.py $data -a $model -j 4 -C -g 0 1 -b 256 -T --ckpt "ckpt_best_v2qq_q"$qb"88.pth" -N -v v2qq --qb $qb --lr 0.01
                 rm -f checkpoint/*/*/ckpt_rt*_v*_q*_epoch_*.pth
-                python3 main.py $data -a $model -j 4 -C -g 0 -E -N --ckpt "ckpt_rt1_v2qq_q"$qb"_best.pth" --qb $qb
+                python3 main.py $data -a $model -j 4 -C -g 0 -E -N --ckpt "ckpt_rt1_v2qq_q"$qb"88_best.pth" --qb $qb
             fi
         done
     done
