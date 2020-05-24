5258b301171f6cfb7b73a03b0d639622962d81ee
LEE KANG HO <ho7719@gmail.com>
2020-05-24 17:36:12 +0000

    update shell

--- down_ckpt_all.sh
+++ down_ckpt_all.sh
@@ -3,27 +3,24 @@
 # for model in mobilenet mobilenetv2 resnet resnext wideresnet vgg
 for model in mobilenet mobilenetv2 resnet
 do
+    echo $model
     for data in cifar10 cifar100
     do
         if [ "$model" = "resnet" ]; then
-            echo "resnet"
             for layer in 20 32 44 56 110
             do
                 python3 down_ckpt.py $data -a $model --layers $layer -o ckpt_best.pth
             done
         else
-            echo $model
             python3 down_ckpt.py $data -a $model -o ckpt_best.pth
         fi
     done
     if [ "$model" = "resnet" ]; then
-        echo "resnet"
         for layer in 18 34 50 101 152
         do
             python3 down_ckpt.py imagenet -a $model --layers $layer -o ckpt_best.pth
         done
     else
-        echo $model
         python3 down_ckpt.py imagenet -a $model -o ckpt_best.pth
     fi
 done
