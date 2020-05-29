a1af24413447d8ac2e7aa7ccd7146ccb42e3a428
LEE KANG HO <ho7719@gmail.com>
2020-05-29 00:41:20 +0000

    fix code

--- find_similar_kernel.py
+++ find_similar_kernel.py
@@ -167,7 +167,6 @@ def find_kernel_pw(model, opt):
     ref_norm_sq = (ref_norm * ref_norm).sum(dim=1)
 
     epsilon = opt.epsilon # epsilon for non-zero denom (default: 1e-5)
-    denom = ref_norm_sq + epsilon
 
     for i in tqdm(range(1, num_layer), ncols=80, unit='layer'):
         idx = []
@@ -182,7 +181,7 @@ def find_kernel_pw(model, opt):
             cur_norm = cur_weight - cur_mean
 
             numer = torch.matmul(cur_norm, ref_norm.T)
-            denom = denom.expand_as(numer)
+            denom = ref_norm_sq.expand_as(numer) + epsilon
             alphas = deepcopy(numer / denom)
             del numer, denom
             betas = cur_mean - alphas * ref_mean.view(-1, ref_length).expand_as(alphas)
