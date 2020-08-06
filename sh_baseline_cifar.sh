40c8c1727ba94c781a20b4d25d2a53efcb519214
LEE KANG HO <ho7719@gmail.com>
2020-08-06 07:36:13 +0000

    Update for baseline test with various weight decay rate

--- sh_baseline_cifar.sh
+++ sh_baseline_cifar.sh
@@ -9,12 +9,12 @@ do
             if [ "$model" = "resnet" ]; then
                 for layer in 20 32 44 56 110
                 do
-                    python3 main.py $data -a $model --layers $layer --datapath /dataset/CIFAR -j 8 -C -g "$1" --wd $wd
+                    python3 main.py $data -a $model --layers $layer --datapath /dataset/CIFAR -j 8 -C -g "$1" --wd $wd --basetest
                     rm -f ./checkpoint/*/*/ckpt_wd*-*_epoch_*.pth
                     python3 main.py $data -a $model --layers $layer --datapath /dataset/CIFAR -j 8 -C -g "$1" -E --ckpt "ckpt_wd"$wd"_best.pth"
                 done
             else
-                python3 main.py $data -a $model --datapath /dataset/CIFAR -j 8 -C -g "$1" --wd $wd
+                python3 main.py $data -a $model --datapath /dataset/CIFAR -j 8 -C -g "$1" --wd $wd --basetest
                 rm -f ./checkpoint/*/*/ckpt_wd*-*_epoch_*.pth
                 python3 main.py $data -a $model --datapath /dataset/CIFAR -j 8 -C -g "$1" -E --ckpt "ckpt_wd"$wd"_best.pth"
             fi
