87e1d228f40a8336a17d64274835f677fe8bfac5
LEE KANG HO <ho7719@gmail.com>
2020-08-09 19:38:55 +0000

    fix erreta: apply tvloss

--- sh_v2_cifar_tvl_warm_run.sh
+++ sh_v2_cifar_tvl_warm_run.sh
@@ -13,15 +13,15 @@ do
                     for layer in 20 32 44 56 110
                     do
                         # python3 main.py $data -a $model --layers $layer --datapath /dataset/CIFAR -j 8 -C -g 0 1 2 3 -N -v v2 -s 1 -warm $warm
-                        python3 main.py $data -a $model --layers $layer --datapath /dataset/CIFAR -j 8 -C -g "$1" -N -v v2 -tvl --tvls $scale -s 1 -warm $warm
+                        python3 main.py $data -a $model --layers $layer --datapath /dataset/CIFAR -j 8 -C -g "$1" -N -v v2 --tvl --tvls $scale -s 1 -warm $warm
                         rm -f ./checkpoint/*/*/ckpt_new_v*_tvl1e-*_s*_warm*_epoch_*.pth
-                        python3 main.py $data -a $model --layers $layer --datapath /dataset/CIFAR -j 8 -C -g "$1" -E -N --ckpt "ckpt_new_v2_s1_warm"$warm"_best.pth"
+                        python3 main.py $data -a $model --layers $layer --datapath /dataset/CIFAR -j 8 -C -g "$1" -E -N --ckpt "ckpt_new_v2_tvl"$scale"_s1_warm"$warm"_best.pth"
                     done
                 else
                     # python3 main.py $data -a $model --datapath /dataset/CIFAR -j 8 -C -g 0 1 2 3 -N -v v2 -s 1 -warm $warm
-                    python3 main.py $data -a $model --datapath /dataset/CIFAR -j 8 -C -g "$1" -N -v v2 --tvls $scale -s 1 -warm $warm
+                    python3 main.py $data -a $model --datapath /dataset/CIFAR -j 8 -C -g "$1" -N -v v2 --tvl --tvls $scale -s 1 -warm $warm
                     rm -f ./checkpoint/*/*/ckpt_new_v*_tvl1e-*_s*_warm*_epoch_*.pth
-                    python3 main.py $data -a $model --datapath /dataset/CIFAR -j 8 -C -g "$1" -E -N --ckpt "ckpt_new_v2_pwd8_pws1_s1_warm"$warm"_best.pth"
+                    python3 main.py $data -a $model --datapath /dataset/CIFAR -j 8 -C -g "$1" -E -N --ckpt "ckpt_new_v2_pwd8_pws1_tvl"$scale"_s1_warm"$warm"_best.pth"
                 fi
             done
         done
