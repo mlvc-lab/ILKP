fa5c22682b14dcdaced7e1508d87652b4116e9fb
LEE KANG HO <ho7719@gmail.com>
2020-09-04 08:30:58 +0000

    fix gpu number

--- sh_v2qq_cifar_test.sh
+++ sh_v2qq_cifar_test.sh
@@ -8,13 +8,13 @@ do
         for qb in 8 7 6 5 4 3 2
         do
             if [ "$model" = "resnet" ]; then
-                for layer in 20 32 44 56 110
-                # for layer in 20 32 44
+                # for layer in 20 32 44 56 110
+                for layer in 20 32 44
                 do
                     echo "v2qq"
                     python3 find_similar_kernel.py $data -a $model --layers $layer --ckpt ckpt_best.pth -v v2qq --qb $qb
                     python3 main.py $data -a $model --layers $layer -j 4 -C -g "$1" -E -N --ckpt "ckpt_best_v2qq_q"$qb"a8b8_eps1e-08.pth" --qb $qb
-                    python3 main.py $data -a $model --layers $layer -j 4 -C -g "$1" 1 -b 256 -T --ckpt "ckpt_best_v2qq_q"$qb"a8b8_eps1e-08.pth" -N -v v2qq --qb $qb --lr 0.01
+                    python3 main.py $data -a $model --layers $layer -j 4 -C -g "$1" -b 256 -T --ckpt "ckpt_best_v2qq_q"$qb"a8b8_eps1e-08.pth" -N -v v2qq --qb $qb --lr 0.01
                     rm -f checkpoint/*/*/ckpt_rt*_v*_q*_eps1e-08_s*_epoch_*.pth
                     python3 main.py $data -a $model --layers $layer -j 4 -C -g "$1" -E -N --ckpt "ckpt_rt1_v2qq_q"$qb"a8b8_eps1e-08_s1_best.pth" --qb $qb
                 done
