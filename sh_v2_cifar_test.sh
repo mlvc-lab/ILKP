c3a52fad92dd47ea1501b535042ef6b83b201b00
LEE KANG HO <ho7719@gmail.com>
2020-09-10 13:43:03 +0000

    Upload shell file for v2 experiments

---
+++ sh_v2_cifar_test.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+for data in cifar10 cifar100
+do
+    # for model in mobilenet mobilenetv2 resnet
+    for model in resnet
+    do
+        if [ "$model" = "resnet" ]; then
+            for layer in 20 32 44 56 110
+            do
+                python3 find_similar_kernel.py $data -a $model --layers $layer --ckpt ckpt_best.pth -v v2
+                python3 main.py $data -a $model --layers $layer -j 4 -C -g "$1" -E -N --ckpt "ckpt_best_v2.pth"
+                python3 main.py $data -a $model --layers $layer -j 4 -C -g "$1" -b 256 -T --ckpt "ckpt_best_v2.pth" -N -v v2 --lr 0.01 --chk-save
+                rm -f ./checkpoint/*/*/ckpt_rt*_v*_s*_epoch_*.pth
+                python3 main.py $data -a $model --layers $layer -j 4 -C -g "$1" -E -N --ckpt "ckpt_rt1_v2_s1_best.pth"
+            done
+        else
+            python3 find_similar_kernel.py $data -a $model --ckpt ckpt_best.pth -v v2
+            python3 main.py $data -a $model -j 4 -C -g "$1" -E -N --ckpt "ckpt_best_v2_pwd8_pws1.pth"
+            python3 main.py $data -a $model -j 4 -C -g "$1" -b 256 -T --ckpt "ckpt_best_v2_pwd8_pws1.pth" -N -v v2 --lr 0.01 --chk-save
+            rm -f ./checkpoint/*/*/ckpt_rt*_v*_pwd8_pws1_s*_epoch_*.pth
+            python3 main.py $data -a $model -j 4 -C -g "$1" -E -N --ckpt "ckpt_rt1_v2_pwd8_pws1_s1_best.pth"
+        fi
+    done
+done
