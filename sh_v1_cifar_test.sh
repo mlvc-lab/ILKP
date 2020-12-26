a0032ed808e59e52023bba8c6b93f176f05a5146
LEE KANG HO <ho7719@gmail.com>
2020-12-26 13:25:26 +0000

    more workers

--- sh_v1_cifar_test.sh
+++ sh_v1_cifar_test.sh
@@ -9,17 +9,17 @@ do
             for layer in 20 32 44 56 110
             do
                 python3 find_similar_kernel.py $data -a $model --layers $layer --ckpt ckpt_best.pth -v v1
-                python3 main.py $data -a $model --layers $layer -j 4 -C -g "$1" -E -N --ckpt "ckpt_best_v1.pth"
-                python3 main.py $data -a $model --layers $layer -j 4 -C -g "$1" -b 256 -T --ckpt "ckpt_best_v1.pth" -N -v v1 --lr 0.01 --chk-save
+                python3 main.py $data -a $model --layers $layer -j 8 -C -g "$1" -E -N --ckpt "ckpt_best_v1.pth"
+                python3 main.py $data -a $model --layers $layer -j 8 -C -g "$1" -b 256 -T --ckpt "ckpt_best_v1.pth" -N -v v1 --lr 0.01 --chk-save
                 rm -f ./checkpoint/*/*/ckpt_rt*_v*_s*_epoch_*.pth
-                python3 main.py $data -a $model --layers $layer -j 4 -C -g "$1" -E -N --ckpt "ckpt_rt1_v1_s1_best.pth"
+                python3 main.py $data -a $model --layers $layer -j 8 -C -g "$1" -E -N --ckpt "ckpt_rt1_v1_s1_best.pth"
             done
         else
             python3 find_similar_kernel.py $data -a $model --ckpt ckpt_best.pth -v v1
-            python3 main.py $data -a $model -j 4 -C -g "$1" -E -N --ckpt "ckpt_best_v1_pwd8_pws1.pth"
-            python3 main.py $data -a $model -j 4 -C -g "$1" -b 256 -T --ckpt "ckpt_best_v1_pwd8_pws1.pth" -N -v v1 --lr 0.01 --chk-save
+            python3 main.py $data -a $model -j 8 -C -g "$1" -E -N --ckpt "ckpt_best_v1_pwd8_pws1.pth"
+            python3 main.py $data -a $model -j 8 -C -g "$1" -b 256 -T --ckpt "ckpt_best_v1_pwd8_pws1.pth" -N -v v1 --lr 0.01 --chk-save
             rm -f ./checkpoint/*/*/ckpt_rt*_v*_pwd8_pws1_s*_epoch_*.pth
-            python3 main.py $data -a $model -j 4 -C -g "$1" -E -N --ckpt "ckpt_rt1_v1_pwd8_pws1_s1_best.pth"
+            python3 main.py $data -a $model -j 8 -C -g "$1" -E -N --ckpt "ckpt_rt1_v1_pwd8_pws1_s1_best.pth"
         fi
     done
 done
